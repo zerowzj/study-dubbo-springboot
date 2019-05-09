cd6e577fb5725bc243cb4be4711305a04a6bcd65
wangzhenjun <wangzhenjun@okay.cn>
2019-05-09 18:32:45 +0800

    更新代码

---
+++ src/main/java/study/dubbo/springboot/Startup.java
@@ -0,0 +1,24 @@
+package study.dubbo.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * 入口
+ *
+ * @author wangzhj
+ */
+@SpringBootApplication
+public class Startup {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Startup.class);
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(Startup.class, args);
+        context.start();
+    }
+}
+
