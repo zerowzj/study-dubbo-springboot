c29be4251c2ae3a887b23cabb577061c556c675f
wangzhenjun <wangzhenjun@okay.cn>
2019-05-09 21:46:46 +0800

    更新代码

--- src/main/java/study/dubbo/springboot/Startup.java
+++ src/main/java/study/dubbo/springboot/Startup.java
@@ -1,10 +1,15 @@
 package study.dubbo.springboot;
 
+import org.apache.dubbo.remoting.http.servlet.DispatcherServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
 
 /**
  * 入口
@@ -19,6 +24,17 @@ public class Startup {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Startup.class, args);
         context.start();
+    }
+
+    @Bean
+    public ServletRegistrationBean backServlet() {
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(dispatcherServlet);
+        registrationBean.setUrlMappings(Arrays.asList("/api/*"));
+        registrationBean.setLoadOnStartup(1);
+
+        return registrationBean;
     }
 }
 
---
+++ src/main/java/study/dubbo/springboot/support/filter/provider/WatchDogFilter.java
@@ -0,0 +1,59 @@
+package study.dubbo.springboot.support.filter.provider;
+
+import com.google.common.base.Joiner;
+import com.google.common.base.Stopwatch;
+import com.google.common.collect.Maps;
+import org.apache.dubbo.common.Constants;
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.Filter;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.RpcResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 看门狗过滤器
+ *
+ * @author wangzhj
+ */
+@Activate(group = Constants.PROVIDER, order = 1)
+public class WatchDogFilter implements Filter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WatchDogFilter.class);
+
+    @Override
+    public Result invoke(Invoker<?> invoker,
+                         Invocation invocation) throws RpcException {
+        //
+        LOGGER.info("i am watch dog");
+        Stopwatch watch = Stopwatch.createStarted();
+        //
+        String canonicalName = invoker.getInterface().getCanonicalName();
+        String methodName = invocation.getMethodName();
+        String fullMethod = Joiner.on(".").join(canonicalName, methodName);
+        URL url = invoker.getUrl();
+        LOGGER.info("url={}", url);
+        //
+        Result result = null;
+        try {
+            result = invoker.invoke(invocation);
+        } catch (Exception ex) {
+//            ex.printStackTrace();
+            Map<String, Object> data = Maps.newHashMap();
+            data.put("code", "9999");
+            data.put("desc", "异常");
+            ((RpcResult) result).setValue(data);
+            return result;
+        } finally {
+            LOGGER.info("[{}] [COST TIME {}ms]", fullMethod, watch.elapsed(TimeUnit.MILLISECONDS));
+        }
+        return result;
+    }
+}
