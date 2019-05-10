6db34e47c1fe84b27c3efd1ffc57bb194ac03164
wangzhenjun <wangzhenjun@okay.cn>
2019-05-10 15:25:38 +0800

    更新代码

--- src/main/java/study/dubbo/springboot/support/config/DispatcherServletCfg.java
+++ src/main/java/study/dubbo/springboot/support/config/DispatcherServletCfg.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import java.util.Arrays;
 
 @Configuration
-public class DispatcherServletCfg {
+public class DispatcherServlProtocolConfigetCfg {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DispatcherServletCfg.class);
 
