8394b9f24c392306a160e550634660c47b0e4a9d
wangzhenjun <wangzhenjun@okay.cn>
2019-05-10 11:39:36 +0800

    更新代码

--- src/main/java/study/dubbo/springboot/Startup.java
+++ src/main/java/study/dubbo/springboot/Startup.java
@@ -1,16 +1,11 @@
 package study.dubbo.springboot;
 
-import org.apache.dubbo.remoting.http.servlet.DispatcherServlet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Arrays;
 
 /**
  * 入口
@@ -27,16 +22,5 @@ public class Startup {
         ConfigurableApplicationContext context = SpringApplication.run(Startup.class, args);
         context.start();
     }
-
-//    @Bean
-//    public ServletRegistrationBean backServlet() {
-//        DispatcherServlet dispatcherServlet = new DispatcherServlet();
-//        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
-//        registrationBean.setServlet(dispatcherServlet);
-//        registrationBean.setUrlMappings(Arrays.asList("/api/*"));
-//        registrationBean.setLoadOnStartup(1);
-//
-//        return registrationBean;
-//    }
 }
 
---
+++ src/main/java/study/dubbo/springboot/support/config/DispatcherServletCfg.java
@@ -0,0 +1,30 @@
+package study.dubbo.springboot.support.config;
+
+import org.apache.dubbo.remoting.http.servlet.DispatcherServlet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+public class DispatcherServletCfg {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DispatcherServletCfg.class);
+
+    @Bean
+    public ServletRegistrationBean backServlet() {
+
+        LOGGER.info("SSSSSSSSSSSSSSSSSSS");
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean();
+        registrationBean.setServlet(dispatcherServlet);
+        registrationBean.setUrlMappings(Arrays.asList("/*"));
+        registrationBean.setLoadOnStartup(1);
+
+        return registrationBean;
+    }
+}
